680f1f71334c78212d057c0ecac09f4c5f9f5c6e
pepeisthedev <pepeisthedev@gmail.com>
2025-01-18 12:05:50 +0100

    change font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -24,8 +24,12 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+        <head>
+              <link href="https://fonts.googleapis.com/css2?family=Geist:wght@100..900&display=swap" rel="stylesheet" />
+          </head>
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      style={{ fontFamily: "'Geist', sans-serif", fontVariationSettings: "'wdth' 62.5" }}
+        className={`antialiased`}
       >
         {children}
       </body>
